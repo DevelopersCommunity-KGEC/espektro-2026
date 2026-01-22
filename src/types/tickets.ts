622ad534aaef1f587020d8336e6c83ed2aff295b
imdebamrita <imdebamrita@gmail.com>
2026-01-23 01:18:58 +0530

    types defined

---
+++ src/types/tickets.ts
@@ -0,0 +1,14 @@
+import { EventData } from "./events";
+
+export interface TicketData {
+  _id: string;
+  userEmail: string;
+  eventId: EventData; // Populated
+  paymentId: string;
+  qrCodeToken: string;
+  status: "booked" | "checked-in" | "cancelled" | "pending";
+  issueType: "payment" | "manual" | "pass";
+  purchaseDate: string | Date;
+  checkInTime?: string | Date;
+  issuedBy?: string;
+}
