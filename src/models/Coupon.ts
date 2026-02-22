1532430a1780d901a1bf9148cc062912f89625e3
imdebamrita <imdebamrita@gmail.com>
2026-02-22 22:46:08 +0530

    reusable cupoons part 2

--- src/models/Coupon.ts
+++ src/models/Coupon.ts
@@ -3,6 +3,8 @@ import mongoose, { Schema, Document } from "mongoose";
 export interface ICoupon extends Document {
   code: string;
   clubId: string;
+  type: "single-use" | "multi-use";
+  maxUses?: number;
   discountPercentage: number;
   isUsed: boolean;
   usedBy?: string; // User Email
@@ -14,6 +16,12 @@ export interface ICoupon extends Document {
 const CouponSchema: Schema = new Schema({
   code: { type: String, required: true, unique: true },
   clubId: { type: String, required: true }, // 'all' or specific clubId
+  type: {
+    type: String,
+    enum: ["single-use", "multi-use"],
+    default: "single-use",
+  },
+  maxUses: { type: Number, default: 1 },
   discountPercentage: { type: Number, required: true, min: 0, max: 100 },
   isUsed: { type: Boolean, default: false },
   usedBy: { type: String },
